90c7212a52914309067a552c8e09234ddb7779d2
pmaiko <petyamaiko@gmail.com>
2020-02-18 12:02:30 +0200

    feat(project): deploy

---
+++ client/build/precache-manifest.df65983787c2e150d043f21e06da6dbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc9c6e8fa1097636d8b7b112bb008e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0da88c19395159624451",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "39d260c4d77385c37296",
+    "url": "/static/css/main.455a3123.chunk.css"
+  },
+  {
+    "revision": "0da88c19395159624451",
+    "url": "/static/js/2.a0a2e6a4.chunk.js"
+  },
+  {
+    "revision": "2ba2081b6c7df8ecdcca12487b4ce022",
+    "url": "/static/js/2.a0a2e6a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39d260c4d77385c37296",
+    "url": "/static/js/main.242107b6.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "f022d9a7055711bdee21b42afc0689de",
+    "url": "/static/media/background.f022d9a7.jpg"
+  }
+]);
